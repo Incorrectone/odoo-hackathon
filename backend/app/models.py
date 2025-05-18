2c5fc3abb6c6048ef10894957475ead5feb1030f
Incorrectone <incorrectmail101@gmail.com>
2025-05-18 10:49:10 +0530

    Created Basic Models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,100 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, AnonymousUserMixin
 from sqlalchemy import CheckConstraint
 from werkzeug.security import generate_password_hash, check_password_hash
 import hashlib
 
 from . import db
+
+class User(db.Model):
+    __tablename__ = 'User'
+    ID = db.Column(db.Integer, primary_key=True, autoincrement=True)                                           # ID INT PRIMARY KEY
+    Name = db.Column(db.String(255), nullable=False)                                                           # Name VARCHAR(255) NOT NULL
+    Mail = db.Column(db.String(255), nullable=False, unique=True, index=True)                                  # Mail VARCHAR(255) NOT NULL
+    Service_ID = db.Column(db.Integer, nullable=False)                            
+    ProfilePIC = db.Column(db.String(255))                                                                     # ProfilePIC VARCHAR(255)
+    About_Me = db.Column(db.Text, nullable=True)                                                               # About_me TEXT
+    Date_Created = db.Column(db.DateTime, default=db.func.now())                                               # Date_Created DATE
+    Password_hash = db.Column(db.String(255), nullable=False)                                                  # Password_hash VARCHAR(255) NOT NULL
+
+    def to_json(self):
+
+        json_project = {
+            'ID': self.ID,
+            'Name': self.Name,
+            'Mail': self.Mail,
+            'Service_ID': self.Service_ID,
+            'ProfilePIC': self.ProfilePIC,
+            'About_Me': self.About_Me,
+            'Date_Created': self.Date_Created,
+        }
+
+        return json_service
+
+    def __repr__(self):
+        return "User: {} [{}]".format(self.Name, self.ID)
+    
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+    
+    @password.setter
+    def password(self, password):
+        self.Password_hash = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.Password_hash, password)
+    
+    def get_id(self):
+        return self.ID
+
+    def is_user(self):
+        return not (self.is_admin())
+    
+    def is_admin(self):
+        if self.Mail == "admin@admin.net":
+            return True
+        return False
+
+    user_in_groups = db.relationship('UserInGroup', backref='user')
+    
+
+class Project(db.Model):
+    __tablename__ = 'Project'
+    ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    Project_Name = db.Column(db.String(255), nullable=False)    
+    Project_Status = db.Column(db.String(20), nullable=False, default='Incomplete')
+    Project_Percentage = db.Column(db.Integer)
+    Date_Created = db.Column(db.DateTime, default=db.func.now())
+    Description = db.Column(db.Text, nullable=True)
+
+    CheckConstraint("Project_Status IN ('Incomplete', 'Completed', 'On Hold')")
+
+    def to_json(self):
+
+        json_project = {
+            'ID': self.ID,
+            'Project_Name': self.Project_Name,
+            'Project_Status': self.Project_Status,
+            'Project_Percentage': self.Project_Percentage,
+            'Date_Created': self.Date_Created,
+            'Description': self.Description
+        }
+
+        return json_service
+    
+    def __repr__(self):
+        return "Project: {} [{}]".format(self.Project_Name, self.ID)
+
+    user_in_groups = db.relationship('UserInGroup', backref='project')
+
+class UserInGroup(db.Model):
+    __tablename__ = 'UserInGroup'
+    ID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    User_ID = db.Column(db.Integer, db.ForeignKey('User.ID'), nullable=False)
+    Project_ID = db.Column(db.Integer, db.ForeignKey('Project.ID'), nullable=False)
+    Admin = db.Column(db.Boolean, default=False, nullable=False) 
+    
+    __table_args__ = (db.UniqueConstraint('User_ID', 'Project_ID', name='unique_user_project'),)
+
+
+
